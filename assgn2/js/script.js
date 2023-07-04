50067e9fbc69fa2fc542a320d8227d4e0822b0f6
Anne <steamengine.a@gmail.com>
2023-07-03 22:37:43 -0400

    Update script.js

--- assgn2/js/script.js
+++ assgn2/js/script.js
@@ -1,66 +1,78 @@
-// use a Class statement with a Constructor to save all the planet information
-// from the JSON file into an array.
 class Planet {
-  constructor(name, image, description) {
-    this.name = name;
+  constructor(
+    planetName,
+    planetColor,
+    planetRadiusKM,
+    distInMillionsKM,
+    image
+  ) {
+    this.name = planetName;
+    this.color = planetColor;
+    this.radius = planetRadiusKM;
+    this.distanceFromSun = distInMillionsKM.fromSun;
+    this.distanceFromEarth = distInMillionsKM.fromEarth;
     this.image = image;
-    this.description = description;
   }
 }
 
-const planetArray = [];
+$(document).ready(function () {
+  var planetsArray = [];
 
-// Fetch the JSON file
-fetch("dataFiles/planets.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((item) => {
-      const planet = new Planet(item.name, item.image, item.description);
-      planetArray.push(planet);
-    });
+  $.ajax({
+    url: "dataFiles/planets.json",
+    dataType: "json",
+    success: function (data) {
+      $.each(data.solarSystem.planets, function (index, planetData) {
+        var planet = new Planet(
+          planetData.planetName,
+          planetData.planetColor,
+          planetData.planetRadiusKM,
+          planetData.distInMillionsKM,
+          planetData.image
+        );
+        planetsArray.push(planet);
+      });
 
-    // Call a function to render the planet list
-    renderPlanetList();
-  })
-  .catch((error) => {
-    console.error("Error:", error);
+      var planetList = $("#planet-list");
+      $.each(planetsArray, function (index, planet) {
+        var listItem = $("<li>").append(
+          $("<a>")
+            .attr("href", "planet.html?id=" + index)
+            .text(planet.name)
+        );
+        planetList.append(listItem);
+      });
+
+      planetList.on("click", "a", function (event) {
+        event.preventDefault();
+        var planetIndex = $(this).attr("href").split("=")[1];
+        var selectedPlanet = planetsArray[planetIndex];
+
+        // Save selected planet information in local storage
+        localStorage.setItem("selectedPlanet", JSON.stringify(selectedPlanet));
+
+        // Redirect to individual page for the selected planet
+        window.location.href = "/html/individual.html";
+      });
+    },
   });
+});
 
-function renderPlanetList() {
-  const planetListContainer = document.getElementById("planet-list");
+$(document).ready(function () {
+  // Retrieve planet data from local storage
+  var selectedPlanetData = JSON.parse(localStorage.getItem("selectedPlanet"));
 
-  planetArray.forEach((planet, index) => {
-    // Create an anchor tag with the planet image and name
-    const planetLink = document.createElement("a");
-    planetLink.href = "#"; // Replace with the URL of the individual planet page
-    planetLink.id = `planet-${index}`; // Set a unique ID based on the index
-    planetLink.addEventListener("click", handleClick); // Add a click event listener
-
-    const planetImage = document.createElement("img");
-    planetImage.src = `images/${planet.image}`;
-    planetImage.alt = planet.name;
-
-    const planetName = document.createElement("span");
-    planetName.textContent = planet.name;
-
-    planetLink.appendChild(planetImage);
-    planetLink.appendChild(planetName);
-
-    // Append the anchor tag to the planet list container
-    planetListContainer.appendChild(planetLink);
-  });
-}
-
-function handleClick(event) {
-  event.preventDefault();
-
-  const planetId = event.target.id;
-  const planetIndex = parseInt(planetId.split("-")[1]);
-  const selectedPlanet = planetArray[planetIndex];
-
-  // Save the selected planet information to local storage
-  localStorage.setItem("selectedPlanet", JSON.stringify(selectedPlanet));
-
-  // Redirect to the individual planet page
-  window.location.href = "individual.html"; // Replace with the URL of the individual planet page
-}
+  // Display the planet details on the individual page
+  $("#planet-name").text(selectedPlanetData.name);
+  $("#planet-color").text("Color: " + selectedPlanetData.color);
+  $("#planet-image").attr("src", "images/" + selectedPlanetData.image);
+  $("#planet-radius").text("Radius: " + selectedPlanetData.radius + " km");
+  $("#planet-distance-sun").text(
+    "Distance from Sun: " + selectedPlanetData.distanceFromSun + " million km"
+  );
+  $("#planet-distance-earth").text(
+    "Distance from Earth: " +
+      selectedPlanetData.distanceFromEarth +
+      " million km"
+  );
+});
